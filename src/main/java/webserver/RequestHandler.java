329f92bc3f9bef049e867f944c831fccf4f82a82
impala <dla4051@naver.com>
2024-04-08 16:24:03 +0900

    feat(RequestHandler) : GET /index.html 응답 프로토타입 작성
    최소 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,12 +22,25 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = reader.readLine();
+
+            // path 가져오기
+            String path = "";
+            if (!"".equals(line)) {
+                String[] headerLine = line.split(" ");
+                path = headerLine[1];
+            }
+
+            // index.html 읽기
+            byte[] bytes = FileIoUtils.loadFileFromClasspath("./templates" + path);
+
+            // response message 만들기
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
+        } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
