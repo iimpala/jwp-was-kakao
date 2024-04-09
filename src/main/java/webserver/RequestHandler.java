b06950703717d9bb6944dc154deb66cafff597ad
impala <dla4051@naver.com>
2024-04-09 15:26:27 +0900

    refactor(RequestHandler): HttpRequestParser를 통한 코드 리팩토링
    기존과 동일한 기능을 지원하면서 가독성을 높이도록 리팩토링

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,15 +3,13 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-import db.DataBase;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
+import utils.HttpRequestParser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,67 +27,42 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String line = reader.readLine();
 
-            // path 가져오기
-            String path = "";
-            if (!"".equals(line)) {
-                String[] headerLine = line.split(" ");
-                path = URLDecoder.decode(headerLine[1], StandardCharsets.UTF_8);
+            List<String> headerLines = getHeaderLines(reader);
+            HttpRequest request = HttpRequestParser.parse(headerLines);
+            String requestUri = request.getRequestUri();
+            String extension = HttpRequestParser.parseExt(requestUri);
+
+            String pathPrefix = "html".equals(extension) ? "./templates" : "./static";
+
+            byte[] bytes = "".getBytes();
+            if (extension != null) {
+                bytes = FileIoUtils.loadFileFromClasspath(pathPrefix + requestUri);
             }
 
-            if (path.contains("?")) {
-                //user 회원가입
-                String[] userUrl = path.split("\\?");
-                String userInfo = userUrl[1];
-
-                String userId = userInfo.split("\\&")[0].split("=")[1];
-                String password = userInfo.split("\\&")[1].split("=")[1];
-                String name = userInfo.split("\\&")[2].split("=")[1];
-                String email = userInfo.split("\\&")[3].split("=")[1];
-
-                User user = new User(userId, password, name, email);
-                DataBase.addUser(user);
-
-                System.out.println("found: " + DataBase.findUserById(userId));
-                byte[] bytes = "ok".getBytes();
-                String ext = "text";
-
-                // response message 만들기
-                DataOutputStream dos = new DataOutputStream(out);
-                response200Header(dos, bytes.length, ext);
-                responseBody(dos, bytes);
-
-            } else {
-                String[] parsedPath = path.split("\\.");
-                String ext = parsedPath[parsedPath.length - 1];
-
-                String pathPrefix = "./";
-                if ("html".equals(ext)) {
-                    pathPrefix += "templates";
-                } else {
-                    pathPrefix += "static";
-                }
-
-
-                // index.html 읽기
-                byte[] bytes = FileIoUtils.loadFileFromClasspath(pathPrefix + path);
-
-                // response message 만들기
-                DataOutputStream dos = new DataOutputStream(out);
-                response200Header(dos, bytes.length, ext);
-                responseBody(dos, bytes);
-            }
-
+            DataOutputStream dos = new DataOutputStream(out);
+            response200Header(dos, bytes.length, extension);
+            responseBody(dos, bytes);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
+    private List<String> getHeaderLines(BufferedReader reader) throws IOException {
+        List<String> headerLines = new ArrayList<>();
+
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            headerLines.add(line);
+        }
+
+        return headerLines;
+    }
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String ext) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/"+ ext +";charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: text/" + ext + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
