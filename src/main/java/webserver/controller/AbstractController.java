6d0340310347e49a1b39f713279b0e889ba22329
impala <dla4051@naver.com>
2024-04-17 15:39:13 +0900

    refactor(AbstractController): controller에서 예외 발생시 기본적으로 400 Bad Request를 응답하도록 변경
    Controller의 예외처리를 위해 AbstractController에 예외 응답 변환 로직을 추가

--- src/main/java/webserver/controller/AbstractController.java
+++ src/main/java/webserver/controller/AbstractController.java
@@ -1,13 +1,25 @@
 package webserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
 import webserver.http.response.HttpResponseFactory;
 
-public class AbstractController implements Controller{
+public class AbstractController implements Controller {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractController.class);
 
     @Override
     public HttpResponse service(HttpRequest request) {
+        try {
+            return doService(request);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return HttpResponseFactory.badRequest();
+        }
+    }
+
+    private HttpResponse doService(HttpRequest request) {
         if (request.isGet()) {
             return doGet(request);
         }
@@ -18,10 +30,10 @@ public class AbstractController implements Controller{
     }
 
     protected HttpResponse doGet(HttpRequest request) {
-        return HttpResponseFactory.badRequest();
+        throw new IllegalStateException();
     }
 
     protected HttpResponse doPost(HttpRequest request) {
-        return HttpResponseFactory.badRequest();
+        throw new IllegalStateException();
     }
 }
